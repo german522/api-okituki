2a02169d5f15c43c865461d5a6773bd9524d0b40
german522 <germancarroj@gmail.com>
2025-03-27 19:23:14 -0600

    Se implementa el Refresh Token en sus primeras etapas

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.post("/refresh-token", authController.refreshToken);
+router.post("/logout", authController.logout);
 router.get("/perfil", authMiddleware, authController.perfil); // Ruta protegida
 
 module.exports = router;
