775c6fc8b0de26827f66fdd89168e9fa6238e9f3
german522 <germancarroj@gmail.com>
2025-04-02 19:03:23 -0600

    Se añade el enviroment de producción para Railway

--- config/config.js
+++ config/config.js
@@ -6,6 +6,15 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
+    port: process.env.PROD_DB_PORT,
     dialect: process.env.DB_DIALECT
   }
 };
