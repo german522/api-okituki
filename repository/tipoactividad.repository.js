424537a10c43dc9d44b16fe539e38e08232b5a77
german522 <germancarroj@gmail.com>
2025-04-10 19:56:57 -0600

    Se añaden actividades y descripcion_actividades a Get Tipo Actividad by Id

--- repository/tipoactividad.repository.js
+++ repository/tipoactividad.repository.js
@@ -1,4 +1,4 @@
-const { TipoActividad } = require("../models");
+const { TipoActividad, Actividad, DescripcionActividad } = require("../models");
 
 class TipoActividadRepository {
     async getAll() {
@@ -6,7 +6,22 @@ class TipoActividadRepository {
     }
 
     async getById(id) {
-        return await TipoActividad.findByPk(id);
+        return await TipoActividad.findOne({
+            where: { id },
+            include: [
+                {
+                    model: Actividad,
+                    as: "actividades",
+                    include: [
+                        {
+                            model: DescripcionActividad,
+                            as: "descripciones",
+                            order: [["numero_paso", "ASC"]]
+                        }
+                    ]
+                }
+            ]
+        });
     }
 }
 
