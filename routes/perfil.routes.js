28313b4794040a66e44083fba87ca529d1d25cbf
german522 <germancarroj@gmail.com>
2025-04-10 21:50:19 -0600

    Update by id se convierte a Update by JWT y se protege la ruta con el módulo de autenticación

--- routes/perfil.routes.js
+++ routes/perfil.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const perfilController = require("../controllers/perfil.controller");
+const authMiddleware = require("../middlewares/authMiddleware");
 
-// Actualiza usuario + persona
-router.put("/:id", perfilController.updatePerfil);
+// Ruta protegida, sin pasar el ID por parámetro
+router.put("/", authMiddleware, perfilController.updatePerfil);
 
 module.exports = router;
