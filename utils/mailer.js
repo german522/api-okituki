d1181f6859188b9044248f9c351c6da770c99258
german522 <germancarroj@gmail.com>
2025-08-12 16:05:34 -0600

    Se actualiza la contraseña de aplicación para la autenticación de la cuenta debido a un cambio de contraseña. Nueva app llamada nodemailerOkiTuki.

--- utils/mailer.js
+++ utils/mailer.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'okitukisaludmental@gmail.com',
-        pass: 'jsfhzflhhizfxhux',
+        pass: 'rvkwtufutfjpqpuq',
     },
 });
 
